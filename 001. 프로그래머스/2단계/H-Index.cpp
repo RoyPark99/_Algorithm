ae2c41ecefa1e0a4d316d43caf9b43210618006d
RoyPark99 <greinkeee@naver.com>
2022-04-06 11:09:48 +0900

    20220406

--- 001. 프로그래머스/2단계/H-Index.cpp
+++ 001. 프로그래머스/2단계/H-Index.cpp
@@ -18,3 +18,32 @@ int solution(vector<int> c) {
     // 커지는 시점이 없으면 전체 사이즈
     return c.size();
 }
+
+
+/* 다른사람 풀이 참고
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+
+    sort(citations.begin(), citations.end(), greater<int>()); // 내림차순 정렬
+    
+    for(int i = citations[0]; i > 0; i--)
+    {
+        int count = 0;
+        for(auto & elem : citations)
+        {
+            if (elem >= i)
+            {
+                count++;
+                if (i == count)
+                    return count;
+            }
+        }
+    }
+}
+*/
